fd54d3d1944e302aaced890a79aa4cc8f022168a
Alesaiko <solcmdr@gmail.com>
2020-02-02 20:10:24 +0300

    drm/msm/sde: Introduce KCAL color control
    Use PCC and HSIC interfaces to manually calibrate the colors of the
    DSI panel via sysfs.  This is a simple driver which uses predefined
    API to do this. As it is a purely platform driver, sysfs is located
    at /sys/devices/platform/kcal_ctrl.0
    
    Supported PCC and HSIC features:
    - RGB control,
    - Color distortion control,
    - Saturation intensity control,
    - Contrast intensity control,
    - Color brightness control,
    - Grayscale mode,
    - Astronomy mode;
    
    We already have atomic ops to alter DSI colors, but users prefer
    this interface over that.
    
    Signed-off-by: Alesaiko <solcmdr@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -13,6 +13,7 @@
 #include "sde_reg_dma.h"
 #include "sde_hw_reg_dma_v1_color_proc.h"
 #include "sde_hw_color_proc_common_v4.h"
+#include "sde_hw_kcal_ctrl.h"
 
 /* Reserve space of 128 words for LUT dma payload set-up */
 #define REG_DMA_HEADERS_BUFFER_SZ (sizeof(u32) * 128)
@@ -939,11 +940,23 @@ static void _dspp_pccv4_off(struct sde_hw_dspp *ctx, void *cfg)
 		DRM_ERROR("failed to kick off ret %d\n", rc);
 }
 
+static inline void
+reg_dmav1_setup_dspp_pa_hsicv17_kcal(struct sde_hw_dspp *ctx, void *ctl)
+{
+	struct drm_msm_pa_hsic hsic_cfg = sde_hw_kcal_hsic_struct();
+	int rc;
+
+	rc = reg_dmav1_setup_dspp_pa_hsicv17_apply(ctx, &hsic_cfg, ctl);
+	if (rc)
+		pr_err("kernel hsic application failed ret %d\n", rc);
+}
+
 void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 {
 	struct sde_hw_reg_dma_ops *dma_ops;
 	struct sde_reg_dma_kickoff_cfg kick_off;
 	struct sde_hw_cp_cfg *hw_cfg = cfg;
+	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pcc *pcc_cfg;
 	struct drm_msm_pcc_coeff *coeffs = NULL;
@@ -1015,6 +1028,10 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 		data[i + 3] = coeffs->r;
 		data[i + 6] = coeffs->g;
 		data[i + 9] = coeffs->b;
+
+		if (kcal->enabled)
+			sde_hw_kcal_pcc_adjust(data, i);
+
 		data[i + 12] = coeffs->rg;
 		data[i + 15] = coeffs->rb;
 		data[i + 18] = coeffs->gb;
@@ -1047,6 +1064,8 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	if (rc)
 		DRM_ERROR("failed to kick off ret %d\n", rc);
 
+	if (kcal->enabled)
+		reg_dmav1_setup_dspp_pa_hsicv17_kcal(ctx, hw_cfg->ctl);
 exit:
 	kfree(data);
 }
@@ -1059,7 +1078,11 @@ void reg_dmav1_setup_dspp_pa_hsicv18(struct sde_hw_dspp *ctx, void *cfg)
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pa_hsic *hsic_cfg;
 	u32 reg = 0, opcode = 0, local_opcode = 0;
+	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
 	int rc;
+
+	if (kcal->enabled)
+		return;
 
 	opcode = SDE_REG_READ(&ctx->hw, ctx->cap->sblk->hsic.base);
 
